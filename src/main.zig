a7e0c2e9a3e53ceef40827317a17dc4c4fefc1ed
inaud <me.kuma3@gmail.com>
2024-06-22 19:08:39 +0900

    Create main.zig

---
+++ src/main.zig
@@ -0,0 +1,20 @@
+const std = @import("std");
+
+pub fn main() !void {
+    const stdout_file = std.io.getStdOut().writer();
+    var bw = std.io.bufferedWriter(stdout_file);
+    const stdout = bw.writer();
+
+    var sBuf:[100]u8 = undefined;
+    const s1 = "image";
+    const s2 = "jpg";
+    var counter:i32 = 5;
+    _ = &counter;
+    const slsBuf = try std.fmt.bufPrint(&sBuf,"{s}_{}.{s}",.{s1,counter,s2});
+    try stdout.print("\n {s}", .{slsBuf});
+    for(0..5)|i|{
+      try stdout.print("\n {}", .{i});
+    }
+
+    try bw.flush(); // don't forget to flush!
+}
